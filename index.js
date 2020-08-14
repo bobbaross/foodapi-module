1ec294541dbb870fef4136fd4e6ac3c8a69ac453
bobbaross <67179348+bobbaross@users.noreply.github.com>
2020-08-14 00:57:49 -0400

    it's working now.

---
+++ index.js
@@ -0,0 +1,48 @@
+const toast = require('./data/toast.json')
+
+
+
+const foodList = ['toast']
+
+exports.foodList = () => {
+    return foodList;
+}
+
+exports.randomFood = () => {
+    return foodList[Math.floor(Math.random() * Object.keys(foodList).length)]
+}
+
+exports.getFact = function (food) {
+    if (!foodList.includes(food.toString())) {
+        return console.warn('Not a valid food.')
+    } else {
+        let chosenFood
+        switch (food) {
+            case `toast`:
+                chosenFood = toast
+            break
+            case `cereal`:
+                chosenFood = cereal
+            break
+        }
+            let factObj = chosenFood.facts
+            return factObj[Math.floor(Math.random() * Object.keys(factObj).length)];
+    }
+}
+exports.getImage = function (food) {
+    if (!foodList.includes(food.toString())) {
+        return console.warn('Not a valid food.')
+    } else {
+        let chosenFood
+        switch (food) {
+            case `toast`:
+                chosenFood = toast
+            break
+            case `cereal`:
+                chosenFood = cereal
+            break
+        }
+            let imgObj = chosenFood.images
+            return imgObj[Math.floor(Math.random() * Object.keys(imgObj).length)];
+    }
+}

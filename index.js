737cc1695052660d006b616186897e12a99db62d
bobbaross <67179348+bobbaross@users.noreply.github.com>
2020-08-14 01:15:07 -0400

    minified

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 const toast = require('./data/toast.json')
-
-
-
-const foodList = ['toast']
+const cereal = require('./data/cereal.json')
+const foodList = ['toast', 'cereal']
 
 exports.foodList = () => {
     return foodList;
 }
-
 exports.randomFood = () => {
     return foodList[Math.floor(Math.random() * Object.keys(foodList).length)]
 }
-
 exports.getFact = function (food) {
     if (!foodList.includes(food.toString())) {
         return console.warn('Not a valid food.')
     } else {
         let chosenFood
         switch (food) {
-            case `toast`:
-                chosenFood = toast
-            break
-            case `cereal`:
-                chosenFood = cereal
-            break
+            case `toast`: chosenFood = toast; break
+            case `cereal`: chosenFood = cereal; break
         }
-            let factObj = chosenFood.facts
-            return factObj[Math.floor(Math.random() * Object.keys(factObj).length)];
+        let factObj = chosenFood.facts
+        return factObj[Math.floor(Math.random() * Object.keys(factObj).length)];
     }
 }
 exports.getImage = function (food) {
@@ -35,14 +27,10 @@ exports.getImage = function (food) {
     } else {
         let chosenFood
         switch (food) {
-            case `toast`:
-                chosenFood = toast
-            break
-            case `cereal`:
-                chosenFood = cereal
-            break
+            case `toast`: chosenFood = toast; break
+            case `cereal`: chosenFood = cereal; break
         }
-            let imgObj = chosenFood.images
-            return imgObj[Math.floor(Math.random() * Object.keys(imgObj).length)];
+        let imgObj = chosenFood.images
+        return imgObj[Math.floor(Math.random() * Object.keys(imgObj).length)];
     }
 }
